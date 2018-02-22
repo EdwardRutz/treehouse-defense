b954fe4b5648e12ac938d1becb20a75ad80327cf
EdwardRutz <96d7cf74@opayq.com>
2018-02-22 16:44:18 -0500

    Set-up Map class and constructor

--- Map.cs
+++ Map.cs
@@ -2,6 +2,23 @@
 {
     class Map
     {
+        //These are instance variables because they exist as long as the object exists
+        //Instance variables can be used by other variables and classes
+        //Make the values read only so user can't change
 
+        public readonly int Width;
+        public readonly int Height;
+
+        //Constructor method
+        //User needs to specify the parameters for width and height
+        //These variables with lower case are called method level variables because they only exist inside this method or "variable scope"
+        //Method variables can only be used by the specific method the variables are located in
+        //Make the constructor public so other classes can use it
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }
 }
--- game.cs
+++ game.cs
@@ -2,9 +2,20 @@
 {
     class Game
     {
-        public void Main()
+        public static void Main()
         {
-            Tower tower = new Tower();
+            //The Map class defines what it means to be a Map
+            //To access the map fields, create a map object
+            //Now can access the field using dot notation
+            //map.width can be used like a variable and assign a value
+            //Create a variable area and assign it the area of the grid
+            //Pass in arguements to constructor to initialize it with a width of 8 and height of 5
+
+
+            Map map = new Map(8, 5); 
+            //map.Width = 8;  //Can remove the lines because they are initialized in the constructor
+            //map.Height = 5;
+            int area = map.Width * map.Height;
         }
     }
 }
